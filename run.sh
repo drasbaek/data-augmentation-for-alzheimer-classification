668d5ce8b85f30843fe55d03dc4abd03786d45d0
Anton Drasbæk <202008161@post.au.dk>
2023-05-11 09:36:03 +0200

    added echos

--- run.sh
+++ run.sh
@@ -6,13 +6,22 @@ python3 -m venv aug_alzheimers_env
 source ./aug_alzheimers_env/bin/activate
 
 # install requirements
+echo "Running augmentations..."
 python3 -m pip install -r requirements.txt
 
 # run baseline
+echo "Running augmentations..."
 python3 ./src/main.py
 
 # run augmentations
+echo "Running augmentation: increased_brightness"
 python3 ./src/main.py --brightness_range 1.2 1.7 --name "increased_brightness"
+
+echo "Running augmentation: zca_whitening"
 python3 ./src/main.py --zca_whitening True --name "zca_whitening"
+
+echo "Running augmentation: zoomed_in"
 python3 ./src/main.py --zoom_range 1.2 1.7 --name "zoomed_in"
+
+echo "Running augmentation: zoomed_out"
 python3 ./src/main.py --horizontal_flip True --name "horizontal_flip"
