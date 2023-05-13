b6086cf2453cf60de4b3df75942287641629e696
Anton Drasbæk <202008161@post.au.dk>
2023-05-13 18:14:42 +0200

    changed augmentations

--- run.sh
+++ run.sh
@@ -15,13 +15,13 @@ python3 ./src/main.py --name "no_augmentation"
 
 # run augmentations
 echo "Running augmentation: increased_brightness"
-python3 ./src/main.py --brightness_range 1.3 1.5 --name "increased_brightness"
+python3 ./src/main.py --brightness_range 1.5 1.6 --name "increased_brightness"
 
-echo "Running augmentation: zca_whitening"
-python3 ./src/main.py --zca_whitening True --name "zca_whitening"
+echo "Running augmentation: shear"
+python3 ./src/main.py --shear_range 40 --name "shear"
 
 echo "Running augmentation: zoomed_in"
 python3 ./src/main.py --zoom_range 0.75 0.8 --name "zoomed_in"
 
-echo "Running augmentation: horizontal_flip"
-python3 ./src/main.py --horizontal_flip True --name "horizontal_flip"
+echo "Running augmentation: rotation"
+python3 ./src/main.py --rotation_range 180 --name "rotation"
