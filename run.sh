8f5d07eee14937f12c0d1be73b55277c850d257c
Anton Drasbæk <202008161@post.au.dk>
2023-05-15 08:36:02 +0200

    added deactivate, edited echos

--- run.sh
+++ run.sh
@@ -6,22 +6,27 @@ python3 -m venv aug_alzheimers_env
 source ./aug_alzheimers_env/bin/activate
 
 # install requirements
-echo "Install Requirements"
+echo "[INFO:] Installing Requirements"
 python3 -m pip install -r requirements.txt
 
 # run baseline
-echo "Running non-augmented baseline"
+echo "[INFO:] Running non-augmented baseline"
 python3 ./src/main.py --name "no_augmentation"
 
 # run augmentations
-echo "Running augmentation: increased_brightness"
+echo "[INFO:] Running augmentation: increased_brightness"
 python3 ./src/main.py --brightness_range 1.5 1.6 --name "increased_brightness"
 
-echo "Running augmentation: shear"
+echo "[INFO:] Running augmentation: shear"
 python3 ./src/main.py --shear_range 40 --name "shear"
 
-echo "Running augmentation: zoomed_in"
+echo "[INFO:] Running augmentation: zoomed_in"
 python3 ./src/main.py --zoom_range 0.75 0.8 --name "zoomed_in"
 
-echo "Running augmentation: rotation"
+echo "[INFO:] Running augmentation: rotation"
 python3 ./src/main.py --rotation_range 180 --name "rotation"
+
+echo "[INFO:] All runs complete. Find results in ./out/"
+
+# deactivate virtual environment
+deactivate
